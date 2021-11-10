baedf273779d566518f8a1f9bce2aaeb662f74c9
vishal <vishalrathod7038@gmail.com>
2021-11-10 09:55:51 +0530

    [vishal] Line Comparison Uc1 Calculate length

---
+++ Line_Comparison_UC1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Line_Comparison_UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  x1, y1, x2, y2;
+            double len, length;
+            double point1, point2;
+
+            Console.WriteLine("enter the value point x1  :" );
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value point y1  :");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value point x2  :");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value point y2  :");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+
+            point1 = Math.Pow((x2 - x1), 2);
+            point2 = Math.Pow((y2 - y1), 2);
+            len = point1 + point2;
+            length = Math.Sqrt(len);
+            Console.ReadLine();
+        }
+    }
+}
